758c24bf281a3c37143ee687d1647a21377c540e
abanov20 <asdfgabanov@gmail.com>
2024-11-21 18:51:59 +0600

    Add files via upload

--- GEEKS_LIBRARY/main_page/views.py
+++ GEEKS_LIBRARY/main_page/views.py
@@ -1,38 +1,48 @@
+from lib2to3.fixes.fix_input import context
+
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from datetime import datetime
+from django.views import generic
 from . import models
 
+
+#search button
+class SearchView(generic.ListView):
+    template_name = 'book.html'
+    context_object_name = 'book_list'
+    paginate_by = 5
+
+    def get_queryset(self):
+        return models.Library.objects.filter(title__icontains=self.request.GET.get('q')).order_by('-id')
+
+    def get_context_data(self, *,object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.request.GET.get('q')
+        return context
+
+
+
 #film list
-def books_list_view(request):
-    if request.method == 'GET':
-        book_list = models.Library.objects.filter().order_by('-id')
-        context = {'book_list': book_list}
-        return render(request, template_name='book.html', context=context)
+class BookListView(generic.ListView):
+    template_name = 'book.html'
+    context_object_name = 'book_list'
+    model = models.Library
+
+    def get_queryset(self):
+        return self.model.objects.filter().order_by('-id')
 
 #detail list
-def book_detail_view(request, id):
-    if request.method == 'GET':
-        book_id = get_object_or_404(models.Library, id=id)
-        context = {'book_id': book_id}
-        return render(request, template_name='book_detail.html', context=context)
+class BookDetailView(generic.DetailView):
+    template_name = 'book_detail.html'
+    context_object_name = 'book_id'
 
-# Create your views here.
-def about_me(request):
-    if request.method == 'GET':
-        return HttpResponse('Меня зовут Ариет.Мне 14 лет, я учусь в лицее.Хочу стать backend разработчиком')
+    def get_object(self, **kwargs):
+        book_id = self.kwargs.get('id')
+        return get_object_or_404(models.Library, id=book_id)
+# def book_detail_view(request, id):
+#     if request.method == 'GET':
+#         book_id = get_object_or_404(models.Library, id=id)
+#         context = {'book_id': book_id}
+#         return render(request, template_name='book_detail.html', context=context)
 
-def about_my_pets(request):
-    if request.method == 'GET':
-        pet_name = 'Марсик'
-        url_picture = '<img src = "https://www.purelypetsinsurance.co.uk/media/vxzdtmlz/scottish-fold-2.jpg?format=webp">'
-        pet_url = f'имя:{pet_name}, фото:{url_picture}'
-        return HttpResponse(pet_url)
-
-def system_time(request):
-    if request.method == 'GET':
-        now=datetime.now()
-        time_now = now.strftime("%I:%M:%S %p")
-        date_now = now.strftime("%B %d, %Y")
-        time_date = f'Дата:{date_now} Время:{time_now}'
-        return HttpResponse(time_date)
