e197ae5af4f66051542b4210eddf523896ca0946
abanov20 <asdfgabanov@gmail.com>
2024-11-21 18:54:04 +0600

    Add files via upload

---
+++ GEEKS_LIBRARY/todo/todo/views.py
@@ -0,0 +1,50 @@
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from . import models, forms
+
+# CRUD
+
+#EDIT
+def update_todo_view(request, id):
+    todo_id = get_object_or_404(models.Order, id=id)
+    if request.method == 'POST':
+        form = forms.ToDoForm(request.POST, instance=todo_id)
+        if form.is_valid():
+            form.save()
+            return redirect('todolist')
+    else:
+        form = forms.ToDoForm(instance=todo_id)
+    return render(request, template_name='todo/update_todo.html',
+                  context={
+                      'form': form,
+                      'todo_id': todo_id
+                  })
+
+#DELETE
+def delete_todo_view(request, id):
+    drop_todo = get_object_or_404(models.Order, id=id)
+    drop_todo.delete()
+    return redirect('todolist')
+
+
+
+#READ
+def todo_list(request):
+    if request.method == 'GET':
+        todo_list = models.Order.objects.filter().order_by('-id')
+        return render(request, template_name='todo/todo_list.html',
+                      context={'todo_list': todo_list})
+
+#create todo_
+
+def create_todo_view(request):
+    if request.method == 'POST':
+        form = forms.ToDoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('todolist')
+    else:
+        form = forms.ToDoForm()
+    return render(request, template_name='todo/create_todo.html',
+                  context={'form': form})
+
