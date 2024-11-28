d3de70cdaf82477df6cd71413da9dee73b68c455
abanov20 <asdfgabanov@gmail.com>
2024-11-28 20:13:37 +0600

    Add files via upload

--- GEEKS_LIBRARY/main/settings.py
+++ GEEKS_LIBRARY/main/settings.py
@@ -42,7 +42,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'users.middlewares.AgeClubMiddleware',
+    'users.middlewares.QualificationsClubMiddleware',
 ]
 
 ROOT_URLCONF = 'main.urls'
