3e85c45d025f0f131b2ea82ceb35d271dfe7402b
abanov20 <asdfgabanov@gmail.com>
2024-11-29 19:30:29 +0600

    Update urls.py

--- GEEKS_LIBRARY/mobile_devices/urls.py
+++ GEEKS_LIBRARY/mobile_devices/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.DeviceListView.as_view(), name='device_list'),
     path('book_detail/<int:id>/', views.DeviceDetailView.as_view(), name='detail'),
-    path('all_products/', views.all_products_lists_view, name='all_products_tags'),
+    path('all_products/', views.all_models_lists_view, name='all_products_tags'),
     path('apple_products/', views.Apple_list_view, name='apple_tags'),
     path('xiomi_products/', views.xiomi_list_view, name='xiomi_tags'),
 ]
