60605e8a7aaa3b55307b87c462845aa61b073707
abanov20 <asdfgabanov@gmail.com>
2024-12-10 13:51:18 +0600

    Add files via upload

--- Afisha/eshop/eshop/urls.py
+++ Afisha/eshop/eshop/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('api/v1/movies/<int:id>/', views.movies_detail_list_api_view),
     path('api/v1/directors/<int:id>/', views.director_detail_list_api_view),
     path('api/v1/reviews/<int:id>/', views.reviews_detail_list_api_view),
+    path('api/v1/movies/reviews/', views.movies_reviews_list_api_view),
 ]
